77217dded1571b7492df36f3fac33c2dee9cf603
Tristan Thompson <thomtris@oregonstate.edu>
2019-12-06 15:26:29 -0800

    Basic architecture for gcode-formatter and vision modules.

---
+++ Vision/vision.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+
+#Convert an image matrix to a gcode string.
+#Inputs: 
+#img: an image matrix (such as from a webcam or image file)
+#Outputs:
+#return: a valid gcode string
+def asGCode(img):
+    return ""
