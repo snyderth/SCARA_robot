1841cdf60ab562d564247f425b09b12450c85463
Tristan Thompson <catsrul10@gmail.com>
2019-12-11 01:25:29 -0800

    Got hue coercement working.

--- Vision/vision.py
+++ Vision/vision.py
@@ -1,49 +1,50 @@
 import cv2 as cv
 import numpy as np
-from scipy import spatial as sc
+from functools import reduce
 
-#Convert an image matrix to a gcode string.
-#Inputs: 
-#img: an image matrix (such as from a webcam or image file)
-#hsvPalette: Array of tuples in the form [(h, s, v), ...]
-#Outputs:
-#return: a valid gcode string
-def asGCode(img, hsvPalette):
-    toHSV(img)
-    coerceHSV(img, hsvPalette)
+'''
+Convert image to valid g-code
+@param img RGB space image
+@param huePalette set of hues to look for when performing edge detection
+@returns g-code string
+'''
+def asGCode(img, huePalette):
+    cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    coerceHue(img, huePalette)
 
     return ""
 
 
-def coerceHSV(img, hsvPalette):
-    toHSV(img)
-    colorCoercer = ColorCoerce(hsvPalette)
-    img = colorCoercer.coerceColor(img)
+# Set an RGB image to only have a specific set of hues
+# Inputs:
+def coerceHue(img, huePalette):
+    cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    img = coerceColor(img, huePalette)
     return img
 
 
-def toHSV(img):
-    return cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# Returns a function that determines if value y or z is closer to x
+def closestTo(x):
+    return lambda y, z: y if abs(y - x) < z else z
 
 
-class ColorCoerce:
-    def __init__(self, hsvPalette):
-        self.lookupTree = sc.KDTree(hsvPalette)
-        self.hsvPalette = hsvPalette
-        self.vCoercePixel = np.vectorize(self.coercePixel)
+# Naive implementation of color coercement. Very slow.
+# Inputs:
+# img: image matrix in HSV color space
+# huePalette: an array of hue values to set the image to
+# Outputs:
+# Image with hues coerced to values in huePalette
+def coerceColor(img, huePalette):
+    w, h, c = np.shape(img)
 
-    def coercePixel(self, px):
-        _, index = self.lookupTree.query(px)
-        return self.hsvPalette[index]
+    #If speed is a concern, find a way to do this that is not O(n^3)
+    for py in range(h):
+        for px in range(w):
+            #Get hue
+            pixelHue = img.item((px, py, 0))
 
-    def coerceColor(self, img):
-        h,w,c = np.shape(img)
-        for py in range(h):
-            for px in range(w):
-                pixelColor = img.item((px, py, 2))
-                _, result = self.lookupTree.query(pixelColor)
-                coercedPixel = self.hsvPalette[result]
-                img.setitem((px, py, 2), coercedPixel)
-        return img
+            #Find closest hue value to pixel
+            coercedHue = reduce(closestTo(pixelHue), huePalette)
 
-#       return self.vCoercePixel(img);
+            img.itemset((px, py, 0), coercedHue)
+    return img
--- imagetest.py
+++ imagetest.py
@@ -1,7 +1,14 @@
 from Vision.vision import *
 import cv2 as cv
-img = cv.imread("Vision/testimg.jpg")
-palette = [(0, 60, 60), (50, 60, 60), (90, 60, 60)]
-coerceHSV(img, palette)
+img = cv.imread("Vision/hsv-samples.jpg")
+palette = [0, 50, 220]
+cv. imshow('original', img)
+coerceHue(img, palette)
 
-
+cv.imshow('coerced', img)
+h, s, v = cv.split(img)
+cv.imshow('h', h)
+while 1:
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
