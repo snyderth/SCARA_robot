8921e08af8e7dd826c642e618e0ff80b558c5c45
Tristan Thompson <catsrul10@gmail.com>
2020-02-21 22:45:01 -0800

    Verified functionality of command interface.

--- src/HPS_to_FPGA/CommandCodeStreamer.py
+++ src/HPS_to_FPGA/CommandCodeStreamer.py
@@ -2,6 +2,7 @@ from HPS_to_FPGA.hps_to_fpga_python import *
 from GCode.gcodeParser import commandCode, COMMAND_MAP, GCODE_MAP, commandToGcode
 from GCode.gcodeFormatter import m2
 import os
+from ctypes import *
 
 
 path = os.path.abspath(__file__)
@@ -9,12 +10,11 @@ path = os.path.realpath(path)
 path = os.path.dirname(path)
 path = os.path.join(path, "libhps_to_fpga_c.so")
 
-def streamProcess(FPGACommands, reportDone, reportSend, reportSent):
+def streamProcess(FPGACommands, reportFPGA, reportDone):
     '''
 
     @param FPGACommands: FPGA command values, in order of execution.
-    @param reportSend: Function in the format (int)->None that reports when a value is about to be sent
-    @param reportSent: Function in the format (bool, int)->None that reports when a value has been sent, and if it was successful or not.
+    @param reportFPGA: Function in the format (int)->None that reports any value returned by the FPGA
     @param reportDone: Function in the format ()->None that reports when the FPGA is done executing
     @return: None
     '''
@@ -26,7 +26,14 @@ def streamProcess(FPGACommands, reportDone, reportSend, reportSent):
         if COMMAND_MAP[commandCode(command)] == m2.lower():
             reportDone()
             return
-        reportSend(command)
-        success = mi.writeFifoBlocking(command)
-        reportSent(success == 1, command)
+        print("sending " + commandToGcode(command, 1))
+        mi.writeFifoBlocking(command)
+        print("sent data")
+        success = pointer(c_int(0))
+
+        fpgaReport = mi.readFifoBlocking()
+	
+        print(success.contents.value)
+        
+        reportFPGA(fpgaReport)
 
